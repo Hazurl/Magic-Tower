2cc0fabbefcda8f1a000ecf4d5a328e06afb80d7
Hazurl <ricoujules69@gmail.com>
2017-08-14 21:19:53 +0200

    ressources loader

--- include/Utilities/RessourcesLoader.h
+++ include/Utilities/RessourcesLoader.h
@@ -4,7 +4,36 @@
 #include <SFML/Graphics.hpp>
 #include <frameworkHaz/Interface/RessourceLoader.hpp>
 
-typedef haz::RessourceLoader<sf::Font, std::string, std::string, std::string> FontLoader;
-typedef haz::RessourceLoader<sf::Texture, std::string, std::string, std::string> TextureLoader;
+class FontAllocator : public haz::Allocator<sf::Font, std::string> {
+public:
+    sf::Font* allocate(std::string s) override {
+        auto f = new sf::Font();
+        f->loadFromFile(s);
+        return f;
+    }
+
+    void deallocate (sf::Font* font) override {
+        delete font;
+        font = nullptr;
+    }
+};
+
+class TextureAllocator : public haz::Allocator<sf::Texture, std::string> {
+public:
+    sf::Texture* allocate(std::string s) override {
+        auto f = new sf::Texture();
+        f->loadFromFile(s);
+        return f;
+    }
+
+    void deallocate (sf::Texture* texture) override {
+        delete texture;
+        texture = nullptr;
+    }
+};
+
+typedef haz::RessourceLoader<sf::Font,      std::string,    std::string,    std::string,    FontAllocator>      FontLoader;
+//                           type           argAlloc        Key             scene Key       Allocator
+typedef haz::RessourceLoader<sf::Texture,   std::string,    std::string,    std::string,    TextureAllocator>   TextureLoader;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,6 @@
 int main (int , char** ) {
     srand(time(NULL));
 
-    FontLoader::useAllocator([] (std::string const& path) {
-        auto f = new sf::Font();
-        f->loadFromFile(path);
-        return f;
-    });
-    TextureLoader::useAllocator([] (std::string const& path) {
-        auto t = new sf::Texture();
-        t->loadFromFile(path);
-        return t;
-    });
-
     GameEngine game;
     return game.start();
 }
